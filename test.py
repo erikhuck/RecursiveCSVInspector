e41b409541b26cbdf2384d3d4802b5584c1b4d08
Erik Huckvale <erikhuck@gmail.com>
2019-12-20 22:31:58 -0700

    Debugging travis build

--- test.py
+++ test.py
@@ -1,10 +1,12 @@
 """Module ran on the command line which runs all the tests"""
 
+from os import system
 import sys
 from unittest import TestLoader, TestSuite, TextTestRunner
 
 from strings.general import TEST_DIR, MAIN_NAME
 
+system('echo $SHELL')
 
 if __name__ == MAIN_NAME:
     test_runner: TextTestRunner = TextTestRunner(verbosity=1)
