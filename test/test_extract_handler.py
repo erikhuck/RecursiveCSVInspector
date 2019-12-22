ca48f51366b8cf2abefd47c9b10d800b6c2759fa
Erik Huckvale <erikhuck@gmail.com>
2019-12-21 18:33:57 -0700

    Separated the handler test case classes into separate modules

--- test/test_extract_handler.py
+++ test/test_extract_handler.py
@@ -1,28 +1,11 @@
-"""Module for test cases which test modules in the handler package"""
+"""Module containing the extract handler test case class"""
 
-from argparse import Namespace
 from os.path import isdir, isfile
 from unittest import TestCase
 
 from handler.master_handler import MasterHandler
 from strings.args import EXTRACT_HANDLER_NAME
-from strings.test_data import TEST_DATA_PATH
 from test.utils import TestDataCreator, get_master_handler
-
-
-class TestMasterHandler(TestCase):
-    """Contains tests for the master handler"""
-
-    def test_init(self):
-        """Tests that the master handler is initialized properly"""
-
-        master_handler: MasterHandler = get_master_handler(handler_type=EXTRACT_HANDLER_NAME)
-
-        args: Namespace = master_handler.args
-        self.assertEqual(args.handler_type, EXTRACT_HANDLER_NAME)
-        self.assertEqual(args.data_path, TEST_DATA_PATH)
-
-        # TODO: Add tests for print handler arg parsing
 
 
 class TestExtractHandler(TestCase):
@@ -63,6 +46,3 @@ class TestExtractHandler(TestCase):
         """
 
         return isdir(path) or isfile(path)
-
-
-# TODO: Add test case for print handler
